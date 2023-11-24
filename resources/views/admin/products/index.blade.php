c4052d054ac4e01f730d1d0bb24c5da0d1f44c13
Asrul Said <asrulsaid313@gmail.com>
2023-11-24 23:45:20 +0800

    add admin.products.index blade

---
+++ resources/views/admin/products/index.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            @if (session()->has('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @elseif(session()->has('error'))
+                <div class="alert alert-danger" role="alert">
+                    {{ session('error') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            <div class="card">
+                <div class="card-header">{{ __('Product') }}</div>
+
+                <div class="card-body">
+                    <a href="{{ route('product.create') }}" class="btn btn-primary mb-3">Add Product</a>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                              <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Image</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">SKU</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Action</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                            @forelse ($products as $product)
+                                <tr>
+                                    <th scope="row">{{ $loop->iteration }}</th>
+                                    <td><img src="{{ asset('/storage/products/'.$product->image) }}" alt="" class="img-thumbnail" style="width: 150px"></td>
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ $product->sku }}</td>
+                                    <td> {{ $product->price }} </td>
+                                    <td>{{ $product->description }}</td>
+                                    <td>
+                                        <div class="d-flex">
+                                            <a href="{{ route('product.edit', $product->id) }}" class="btn btn-info">Edit</a>
+                                            <form action="{{ route('product.destroy', $product->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @empty
+                                <td colspan="7" class="text-center">
+                                    No Product
+                                </td>
+                            @endforelse
+                            </tbody>
+                        </table>
+
+                        {{ $products->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
