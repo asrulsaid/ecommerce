45a88a0ff438246ab3e1e7a47a9cd37fd538005b
Asrul Said <asrulsaid313@gmail.com>
2023-11-26 15:22:25 +0800

    edit products.index blade

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -83,7 +83,7 @@
                                 </div>
                                 <!-- Product actions-->
                                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{ route('product.show', $product->id) }}">Detail</a></div>
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="{{ route('products.show', $product->id) }}">Detail</a></div>
                                 </div>
                             </div>
                         </div>
