94d4b3318bcd66ade2e19d270f1fedb5941b5715
Asrul Said <asrulsaid313@gmail.com>
2023-11-26 16:35:10 +0800

    add Authentication and resources

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
+// import facade Storage
+use Illuminate\Support\Facades\Storage;
+
+use Auth;
+
 class ProductController extends Controller
 {
+    public function __construct() {
+        $this->middleware(['auth', 'ensureUserRole'])->except('index', 'show');
+    }
     /**
      * Display a listing of the resource.
      */
@@ -15,7 +23,14 @@ class ProductController extends Controller
         // mengambil data dari model Product
         $products = Product::latest()->paginate(5);
 
-        // menampilkan view homepage beserta data products
+        // cek jika user login dan memiliki role sebagai admin
+        if (Auth::check() && Auth::user()->is_admin) {
+            return view('admin.products.index', [
+                'products' => $products
+            ]);
+        }
+
+        // menampilkan products.index beserta data products
         return view('products.index', ['products' => $products]);
     }
 
@@ -24,7 +39,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.products.create');
     }
 
     /**
@@ -32,7 +47,28 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validasi data
+        $this->validate($request, [
+            'name' => 'required',
+            'sku' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+        ]);
+
+        // menyimpan gambar
+        $image = $request->file('image');
+        $image->storeAs('public/products', $image->hashName());
+
+        Product::create([
+            'name' => $request->name,
+            'sku' => $request->sku,
+            'price' => $request->price,
+            'description' => $request->description,
+            'image' => $image->hashName(),
+        ]);
+
+        return redirect()->route('products.index')->with(['success' => 'Product has been inserted!']);
     }
 
     /**
@@ -40,7 +76,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return view('products.show', ['product' => $product]);
     }
 
     /**
@@ -48,7 +84,7 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        return view('admin.products.edit', ['product' =>$product]);
     }
 
     /**
@@ -56,7 +92,45 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        // validasi data
+        $this->validate($request, [
+            'name' => 'required',
+            'sku' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'image' => 'image|mimes:jpeg,jpg,png|max:2048',
+        ]);
+
+        // cek jika ada gambar yang di upload pada form
+        if ($request->hasFile('image')) {
+
+            // menyimpan gambar baru
+            $image = $request->file('image');
+            $image->storeAs('public/products', $image->hashName());
+
+            // menghapus gambar yang lama
+            Storage::delete('public/products/'.$product->image);
+
+            // mengupdate data product beserta gambar yang baru
+            $product->update([
+                'name' => $request->name,
+                'sku' => $request->sku,
+                'price' => $request->price,
+                'description' => $request->description,
+                'image' => $image->hashName()
+            ]);
+
+        } else {
+            // mengupdate data tanpa gambar baru
+            $product->update([
+                'name' => $request->name,
+                'sku' => $request->sku,
+                'price' => $request->price,
+                'description' => $request->description
+            ]);
+        }
+
+        return redirect()->route('products.index')->with(['success' => 'Product has been updated!']);
     }
 
     /**
@@ -64,6 +138,12 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        // menghapus file gambar
+        Storage::delete('public/products/'. $product->image);
+
+        // menghapus data product di database
+        $product->delete();
+
+        return redirect()->route('products.index')->with(['success' => 'Product has been deleted!']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index']);
+
+Route::resource('products', ProductController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
